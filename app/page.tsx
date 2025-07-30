50d4cbbf574906405423bc9eb46e59e0e0ecf5ff
Ali Arthur <aliarthur35@gmail.com>
2025-07-30 17:18:47 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ async function Home() {
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
         {data}
       </section>
-
       <LoadMore />
     </main>
   );
